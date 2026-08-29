c0b043fff12af4677fb13d87805ef713fe76e3a0
Andrew <andrewmatthian08@gmail.com>
2026-08-28 21:59:17 -0400

    initialized frontend elements

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,18 @@
-from flask import Flask, request
+from pathlib import Path
+from flask import Flask, request, render_template
 from extensions import db
 from models.user import UserModel
 from google_places import search_places
 
-app = Flask(__name__)
+FRONTEND = Path(__file__).resolve().parent.parent / "frontend"
+
+app = Flask(__name__, template_folder=FRONTEND/"templates" , static_folder=FRONTEND/"static")
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 db.init_app(app)
 
 @app.route("/")
 def home():
-    return "MapRank is running"
+    return render_template("home.html")
 
 @app.route("/places/search", methods=['GET'])
 def places_search():
