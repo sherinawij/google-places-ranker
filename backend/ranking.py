e2b6d236f595b3df11b94fdfd4d7f30d5d2290f5
sherina wijaya <sherina200607@gmail.com>
2026-08-29 17:12:43 -0400

    feat: weighted sorting

---
+++ backend/ranking.py
@@ -0,0 +1,19 @@
+m = 100
+
+def weighted_score(rating, review_count, average_rating):
+    v = review_count
+    R = rating
+    C = average_rating
+    return (v/(v+m))*R + (m/(v+m))*C
+
+def add_score(places):
+    ratings = [place['rating'] for place in places]
+    average_rating = sum(ratings)/len(ratings)
+    for place in places:
+        if(place['rating'] is None):
+            place['score'] = None
+        else:
+            rating = place['rating']
+            review_count = place['review_count']
+            place['score'] = weighted_score(rating, review_count, average_rating) 
+    return average_rating
